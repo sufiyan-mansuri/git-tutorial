baa713d1832b8cbb3aae1c04d8f3bf73afe36f50
Sufiyan Mansuri <mansurisufiyan141@gmail.com>
2025-12-18 16:03:59 +0530

    Version 2

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
 console.log("config");
 console.log('version1')
+
+console.log('changes for version 2')
--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 console.log('index.js')
 console.log('version1')
+
+console.log('changes for version 2')
